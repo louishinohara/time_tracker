1eeeaa48725bb2e6852538a0bf7483441eb6fb6d
Ushio Shinohara <44354821+louishinohara@users.noreply.github.com>
2020-01-08 12:19:44 -0500

    Update sign_in_page.dart
    Add Authentication Buttons on Landing Page

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -4,19 +4,57 @@ class SignInPage extends StatelessWidget {
   // Creating a new class for the main.dart
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    var scaffold3 = Scaffold(
       // Used to build the page
       appBar: AppBar(
         title: Text('Time Tracker'), // Text displayed on app bar
         elevation: 2,
       ),
-      body: Container(
-          color: Colors.white,
-          child: Column( // A container can specify a widget child
-            children: <Widget>[ // A column has children which is a list of widgets 
-              Container(color: Colors.orange,)
-            ],
-          )),
+      body: _buildContent(),
+    );
+    var scaffold2 = scaffold3;
+    var scaffold = scaffold2;
+    return scaffold;
+  }
+
+  Widget _buildContent() {
+    // Set return type to be a widget
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        // A container can specify a widget child
+        children: <Widget>[
+          // A column has children which is a list of widgets
+          Container(
+            color: Colors.orange,
+            // Widget for a box. Uses the container and creates individual space on page
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              // Widget for a box. Uses the container and creates individual space on page
+              height: 100,
+            ),
+          ),
+          SizedBox(height: 8),
+          Container(
+            color: Colors.purple,
+            // Widget for a box. Uses the container and creates individual space on page
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+          SizedBox(height: 8),
+        ],
+      ),
     );
   }
 }
