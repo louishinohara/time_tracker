0f3118ed9699056a56d3efcc0bab9da4ab92396b
Ushio Shinohara <44354821+louishinohara@users.noreply.github.com>
2020-01-08 22:12:05 -0500

    Update app/sign_in
    Finished adding all buttons for log in page

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'package:time_tracker/common_widgets/custom_raised_button.dart';
+import './sign_in_button.dart';
+
 class SignInPage extends StatelessWidget {
   // Creating a new class for the main.dart
   @override
   Widget build(BuildContext context) {
-    var scaffold3 = Scaffold(
+    return Scaffold(
       // Used to build the page
       appBar: AppBar(
         title: Text('Time Tracker'), // Text displayed on app bar
         elevation: 2,
       ),
       body: _buildContent(),
+      backgroundColor: Colors.grey[100],
     );
-    var scaffold2 = scaffold3;
-    var scaffold = scaffold2;
-    return scaffold;
   }
 
   Widget _buildContent() {
@@ -22,37 +23,54 @@ class SignInPage extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         // A container can specify a widget child
         children: <Widget>[
           // A column has children which is a list of widgets
-          Container(
-            color: Colors.orange,
-            // Widget for a box. Uses the container and creates individual space on page
-            child: SizedBox(
-              height: 100,
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w600,
             ),
           ),
-          SizedBox(
-            height: 8,
+          SizedBox(height: 48),
+          SignInButton(
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () {},
           ),
-          Container(
-            color: Colors.red,
-            child: SizedBox(
-              // Widget for a box. Uses the container and creates individual space on page
-              height: 100,
-            ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
           ),
-          SizedBox(height: 8),
-          Container(
-            color: Colors.purple,
-            // Widget for a box. Uses the container and creates individual space on page
-            child: SizedBox(
-              height: 100,
-            ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
           ),
-          SizedBox(height: 8),
+          SizedBox(height: 8.0),
+          Text('or',
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.black87,
+              ),
+              textAlign: TextAlign.center),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Go Anonymously',
+            textColor: Colors.white,
+            color: Colors.black45,
+            onPressed: () {},
+          ),
         ],
       ),
     );
---
+++ lib/common_widgets/custom_raised_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomRaisedButton extends StatelessWidget {
+  // Properties in stateless widget must be immutable
+  CustomRaisedButton({
+    this.child,
+    this.color,
+    this.borderRadius: 4.0,
+    this.height = 600.0,
+    this.onPressed,
+  });
+  final Widget child;
+  final Color color;
+  final double borderRadius;
+  final double height;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+          child: RaisedButton(
+        child: child,
+        color: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(borderRadius),
+          ),
+        ),
+        onPressed:
+            onPressed, // () => Paramter/Function and {} is code written (to do)
+      ),
+    );
+  }
+}
